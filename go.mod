15ff476d00db49c48a0a7c6c643fa42b9f387afc
jeremyd <jeremyd>
2024-10-17 07:01:40 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,49 @@
+module github.com/jeremyd/nip17pipe
+
+go 1.22.5
+
+require (
+	github.com/jeremyd/crusher17 v0.0.0-20240729011027-d82e4375428a
+	github.com/nbd-wtf/go-nostr v0.34.4
+	github.com/spf13/cobra v1.8.1
+	github.com/spf13/viper v1.19.0
+)
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.4.0 // indirect
+	github.com/sagikazarmark/locafero v0.6.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tidwall/gjson v1.17.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/jeremyd/crusher17 => ../nip17-crusher
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jeremyd/crusher17"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "app",
+	Short: "A brief description of your application",
+	Long:  `A longer description that spans multiple lines and likely contains examples and usage of using your application.`,
+}
+
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "Send a message",
+	Long:  `Send a message using the provided private key and the recipient's public key.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// Load sender's private key from environment variable
+		sk := viper.GetString("NOSTR_PRIVATE_KEY")
+		if sk == "" {
+			fmt.Println("NOSTR_PRIVATE_KEY not set in the environment")
+			os.Exit(1)
+		}
+
+		message, _ := cmd.Flags().GetString("message")
+		relay, _ := cmd.Flags().GetString("relay")
+		receivers, _ := cmd.Flags().GetStringArray("receiver")
+		fmt.Println("Sending message to", receivers)
+
+		receiverMap := make(map[string]string)
+		for _, pk := range receivers {
+			receiverMap[pk] = relay
+		}
+
+		gwe := &crusher17.GiftWrapEvent{
+			SenderSecretKey: sk,
+			SenderRelay:     relay,
+			ReceiverPubkeys: receiverMap,
+			Content:         message,
+			Subject:         "",
+			GiftWraps:       make(map[string]string),
+		}
+
+		// Wrap the message
+		err := gwe.Wrap()
+		if err != nil {
+			fmt.Printf("Error wrapping message: %v\n", err)
+			return
+		}
+
+		// Print the gift wraps for each receiver
+		for _, giftWrap := range gwe.GiftWraps {
+			fmt.Printf("%s\n", giftWrap)
+		}
+	},
+}
+
+var receiveCmd = &cobra.Command{
+	Use:   "receive",
+	Short: "Receive a message",
+	Long:  `Receive a message using the provided private key and the gift.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		sk := viper.GetString("NOSTR_PRIVATE_KEY")
+		if sk == "" {
+			fmt.Println("NOSTR_PRIVATE_KEY not set in the environment")
+			os.Exit(1)
+		}
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			gift := strings.TrimSpace(scanner.Text())
+			// Your logic here
+			received, err := crusher17.ReceiveMessage(sk, gift)
+
+			if err != nil {
+				fmt.Println("error receiving message: ", err)
+			}
+
+			var ev nostr.Event
+
+			jsonerr := json.Unmarshal([]byte(received), &ev)
+			if jsonerr != nil {
+				fmt.Println("error decoding message: ", jsonerr)
+			}
+
+			fmt.Printf("Message from %s: %s\n", ev.PubKey, ev.Content)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sendCmd)
+	rootCmd.AddCommand(receiveCmd)
+
+	sendCmd.Flags().StringArrayP("receiver", "r", []string{}, "Receiver public key (can be used multiple times)")
+	sendCmd.Flags().StringP("message", "m", "", "Message to send")
+	sendCmd.Flags().StringP("relay", "R", "wss://auth.nostr1.com", "relay to use")
+
+	sendCmd.MarkFlagRequired("message")
+	sendCmd.MarkFlagRequired("receiver")
+
+	viper.AutomaticEnv()
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
